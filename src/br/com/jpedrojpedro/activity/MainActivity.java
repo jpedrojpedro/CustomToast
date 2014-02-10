219cb81f8b2ac2975594b6731f81757bf7906340
jpedrojpedro <joaopedro.pinheiro88@gmail.com>
2014-02-10 17:58:37 -0200

    First Commit!

---
+++ src/br/com/jpedrojpedro/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.jpedrojpedro.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import br.com.jpedrojpedro.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
